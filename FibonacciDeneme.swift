eb909c39649fb4f28555137df99c08532eaf2af9
Humeyra Sahin <bme.humeyrasahin@outlook.com>
2021-12-14 12:45:08 +0300

    first commit

---
+++ FibonacciDeneme.swift
@@ -0,0 +1,24 @@
+//
+//  FibonacciDeneme.swift
+//  
+//
+//  Created by Hümeyra Şahin on 14.12.2021.
+//
+
+var foundArray = [Int]()
+var fibArray = [0,1]
+var myNum = 0
+
+public func fib() {
+    let count = fibArray.count
+    let new =  fibArray[count - 1] + fibArray[count - 2]
+    fibArray.append(new)
+    if new >= 750 && new <= 1000{
+        foundArray.append(new)
+    }
+    myNum = new
+}
+
+while myNum <= 1000 {
+    fib()
+}
